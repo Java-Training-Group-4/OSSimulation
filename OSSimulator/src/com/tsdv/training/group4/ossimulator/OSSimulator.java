015b6c473e34ae2ac4772fa9842a82b74398af5e
ManhLQ <manhlq91@gmail.com>
2016-11-05 23:06:08 +0700

    Add Main class

---
+++ OSSimulator/src/com/tsdv/training/group4/ossimulator/OSSimulator.java
@@ -0,0 +1,15 @@
+package com.tsdv.training.group4.ossimulator;
+
+/**
+ *
+ */
+public class OSSimulator {
+
+    public static void main(String[] args) {
+
+    }
+
+    private int[] loadProgram(String fileName) {
+        throw new UnsupportedOperationException();
+    }
+}

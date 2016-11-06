bf1d7f3a3325d3b47dc281b66b8f01b5e28a9153
tuanbk55 <nhutuan.bk.vn55@gmail.com>
2016-11-06 11:29:15 +0700

    merge develop-tuannn to develop

--- OSSimulator/src/com/tsdv/training/group4/ossimulator/Interrupt.java
+++ OSSimulator/src/com/tsdv/training/group4/ossimulator/Interrupt.java
@@ -1,12 +1,12 @@
 package com.tsdv.training.group4.ossimulator;
 
 /**
- *
+ * Interrupt interface
  */
 public interface Interrupt {
 
     /**
-     *
+     * Interrupt processing
      */
     public void interrupt();
 }
--- OSSimulator/src/com/tsdv/training/group4/ossimulator/TimerInterrupt.java
+++ OSSimulator/src/com/tsdv/training/group4/ossimulator/TimerInterrupt.java
@@ -1,34 +1,66 @@
 package com.tsdv.training.group4.ossimulator;
 
 /**
- *
+ * Timer class.<br>
+ * A timer will interrupt the processor after every X instruction,<br>
+ * where X is a command-line parameter.
  */
 public class TimerInterrupt implements Interrupt {
-
+    /**
+     * counter executed instruction.
+     */
     private int counter;
-    private int timer;
+    /**
+     * number of instruction for a timer.
+     */
+    private int nInstruction;
 
-    public TimerInterrupt() {
+    /**
+     * Constructor.
+     * @param nInstruction number of instruction for a timer.
+     */
+    public TimerInterrupt(int nInstruction) {
+        this.nInstruction = nInstruction;
+        this.counter = 0;
     }
 
+    /**
+     * interrupt procession for timer.
+     */
     @Override
     public void interrupt() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public int getTimer() {
-        return timer;
+    /**
+     * get number of instruction for a timer.
+     * @return number of instruction.
+     */
+    public int getNInstruction() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void setTimer(int timer) {
-        this.timer = timer;
+    /**
+     * set number of instruction for a timer.
+     * @param nInstruction number of instruction.
+     */
+    public void setNInstruction(int nInstruction) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * get number of executed instruction.
+     * @return number of executed instruction.
+     */
     public int getCounter() {
-        return counter;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     * set number of executed instruction.
+     * @param counter number of executed instruction.
+     */
     public void setCounter(int counter) {
-        this.counter = counter;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
